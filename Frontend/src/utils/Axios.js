de5495fc295c7df62d04713a2a5ebeaae86e24d2
vishal kumar <130959006+Vishalpandey1799@users.noreply.github.com>
2025-04-03 17:01:07 +0530

    hey

--- Frontend/src/utils/Axios.js
+++ Frontend/src/utils/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
  
- console.log(import.meta.env.VITE_USER_URL)
+ 
 
 export const axiosInstance = axios.create({
     baseURL :  import.meta.env.VITE_USER_URL||"http://localhost:5000/api",
