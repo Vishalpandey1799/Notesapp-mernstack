bbc9317160ebd62149ca556cf1d83ab92d407629
vishal kumar <130959006+Vishalpandey1799@users.noreply.github.com>
2025-04-03 16:10:39 +0530

    bug fixes

--- Frontend/src/utils/Axios.js
+++ Frontend/src/utils/Axios.js
@@ -1,10 +1,10 @@
 import axios from "axios"
  
 
- 
+//  console.log(import.meta.env.VITE_SERVER_URL)
 
 export const axiosInstance = axios.create({
-    baseURL : import.meta.env.VITE_SERVER_URL,
+    baseURL : "http://localhost:5000/api",
     headers : {
         "Content-Type" : "application/json",
   
